2626464ed37c22241e736ea0b5251a8b3511f8c3
nikhil-sde <nikhil3h@gmail.com>
2025-11-06 21:11:01 -0500

    addingComments
    adding comments in main class of ICE3

--- src/main/java/com/midtermgitcode2/ice3/ICE3.java
+++ src/main/java/com/midtermgitcode2/ice3/ICE3.java
@@ -11,14 +11,18 @@ package com.midtermgitcode2.ice3;
 public class ICE3 {
 
     public static void main(String[] args) {
+        // Create clients
         Client peter = new Client("Peter");
         Client paula = new Client("Paula");
 
+        // Register clients with the LittleGirl
         Client[] clients = {peter, paula};
         LittleGirl littleGirl = new LittleGirl(clients);
 
+        // Register LittleGirl with the Bakery
         Bakery bakery = new Bakery(littleGirl);
 
+        // Start client activities and bakery thread
         littleGirl.startClientActivities();
         bakery.run();
     }
